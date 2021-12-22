d55d626591b771e6ac9ddf9a0cef36ce5cbb9d0c
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2021-12-22 12:15:52 +0700

    feat: private route and simple login UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,20 @@ import ChannelPage from "pages/Channel";
 import DashboardPage from "pages/Dashboard";
 import InboxPage from "pages/Inbox";
 import Compose from "pages/Channel/Compose";
+import PrivateRoute from "routes/PrivateRoute";
 
 function App() {
   return (
     <Routes>
-      <Route caseSensitive path="/a/:workspaceId/" element={<DashboardPage />}>
+      <Route
+        caseSensitive
+        path="/a/:workspaceId/"
+        element={
+          <PrivateRoute>
+            <DashboardPage />
+          </PrivateRoute>
+        }
+      >
         <Route path="inbox" element={<InboxPage />} />
         <Route path="ch/:channelId" element={<ChannelPage />} />
         <Route path="ch/:channelId/compose/:composeId" element={<Compose />} />
