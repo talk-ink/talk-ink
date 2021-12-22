de769b84978efce2e045f16239ebb27d3dcf79f5
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2021-12-22 20:09:34 +0700

    chore: integrate user session when already logged in and add loading indicator to restricted route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router";
+import cookies from "js-cookie";
 
 import NotFound from "components/NotFound/NotFound";
 import ChannelPage from "pages/Channel";
@@ -8,8 +10,38 @@ import Compose from "pages/Channel/Compose";
 import RestrictedRoute from "routes/RestrictedRoute";
 import LoginPage from "pages/Login";
 import RegisterPage from "pages/Register";
+import { useAppDispatch } from "hooks/useAppDispatch";
+import { setAuthLoading, setAuthToken, setAuthUser } from "features/auth";
+import { kontenbase } from "lib/client";
+import { Token, User } from "types";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const checkUser = async () => {
+    try {
+      const cookiesToken: Token = { token: cookies.get("token") };
+      if (!cookiesToken.token) throw new Error("Invalid Token");
+
+      const { data } = await kontenbase.auth.profile();
+
+      if (!data) throw new Error("Invalid user");
+
+      const user: User = data;
+
+      dispatch(setAuthToken(cookiesToken));
+      dispatch(setAuthUser(user));
+    } catch (error) {
+      console.log("err", error);
+    } finally {
+      console.log("awe");
+      dispatch(setAuthLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
   return (
     <Routes>
       <Route
