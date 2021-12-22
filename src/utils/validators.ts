343ba3c4164092b314a8b38a4de133525ee0839e
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2021-12-22 22:21:01 +0700

    chore: change password min char to 6

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -2,11 +2,11 @@ import * as yup from "yup";
 
 export const loginValidation = yup.object().shape({
   email: yup.string().min(8).max(40).required(),
-  password: yup.string().min(8).max(16).required(),
+  password: yup.string().min(6).max(16).required(),
 });
 
 export const registerValidation = yup.object().shape({
   email: yup.string().min(8).max(40).required(),
   firstName: yup.string().min(8).max(80).required(),
-  password: yup.string().min(8).max(16).required(),
+  password: yup.string().min(6).max(16).required(),
 });
