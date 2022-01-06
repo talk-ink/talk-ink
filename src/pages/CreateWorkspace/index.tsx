62bf7c9cc8961d0e3373d2905c94e92eb5087bb3
Fadhil <fadhildarma13@gmail.com>
2022-01-06 20:01:39 +0700

    chore: create workspace responsive

--- src/pages/CreateWorkspace/index.tsx
+++ src/pages/CreateWorkspace/index.tsx
@@ -81,8 +81,8 @@ function CreateWorkspacePage() {
     apiLoading;
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-slightGray">
-      <div className="w-5/12 bg-slate-100 border border-neutral-200 rounded-md px-20 py-16 flex flex-col justify-center">
+    <div className="w-screen h-screen flex items-center justify-center text-slightGray px-5 md:px-0">
+      <div className="w-full md:w-5/12 bg-slate-100 border border-neutral-200 rounded-md px-5 md:px-20 py-16 flex flex-col justify-center">
         <h1 className="text-3xl font-semibold">Create Workspace/Team</h1>
 
         <form onSubmit={formik.handleSubmit} className="mt-8">
