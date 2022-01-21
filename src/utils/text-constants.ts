b96b84aa042c3dacb997416c7ecbba96d793f878
Fadhil <fadhildarma13@gmail.com>
2022-01-21 09:53:11 +0700

    fix: password max length

--- src/utils/text-constants.ts
+++ src/utils/text-constants.ts
@@ -19,17 +19,18 @@ export const SettingsModalHeader: {
 };
 
 export const GreetingThread: string = `
-## What's talk.ink
+## **What's talk.ink**
 Talk.ink is an open source team communication tools
 
-### How it works
+### **How it works**
 Talk.ink has three features:
 
  1. **Threads** let your team to share their opinion, collaborate with other's or share some interesting stuff. You can tag members or mention them in threads so they will notified.
  2. **Channels** organize threads by it's name
  3. **Inbox** will let you know when there is a new thread in channel that you joined, or someone tagged you, and if you feel done with related thread, you can mark it as done so it doesn't disturb you
 
-### Create your first thread
+
+### **Create your first thread**
 If you want to start a new topic, you should create a thread which is really simple
 
  1. Go to preferred channel, and click the **New Thread** button
@@ -37,13 +38,15 @@ If you want to start a new topic, you should create a thread which is really sim
  3. Tag peoples if you want. Also you can tag everyone in the channel or tag specific people
  4. If you done, simply click the post button, that's it! 
 
+
 ![New thread example](https://i.ibb.co/XXccdb5/2022-01-18-134519-1577x934-scrot.png)
 
-### Create your first channel
+### **Create your first channel**
 Start a new and different discussion topic by creating new channel, this is how you do it :
 
  1. Click **New Channel** button on the sidebar, a modal will popup
  2. Fill the channel name, description (optional), and change channel privacy (public by default)
 
+ 
 ![New channel example](https://i.ibb.co/8j2QfVF/2022-01-18-140520-779x340-scrot.png)
 `;
