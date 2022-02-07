ce40ce2a39d40926f4133c55d44f0d919882d1b6
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-02-07 20:35:59 +0700

    chore: image placeholder when still load

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import NameInitial from "./NameInitial";
 
 interface IProps {
   src: string;
   size?: "small" | "medium" | "large";
+  alt?: string;
 }
 
-const Avatar: React.FC<IProps> = ({ src, size = "medium" }) => {
+const Avatar: React.FC<IProps> = ({ src, size = "medium", alt }) => {
+  const [loading, setLoading] = useState<boolean>(true);
   const avatarWidth = {
     small: "h-6 w-6",
     medium: "h-8 w-8 ",
@@ -13,9 +16,23 @@ const Avatar: React.FC<IProps> = ({ src, size = "medium" }) => {
   };
 
   return (
-    <div className={`${avatarWidth[size]} rounded-full overflow-hidden `}>
-      <img src={src} className={`h-full w-full object-cover`} alt="avatar" />
-    </div>
+    <>
+      {loading && <NameInitial size={size} name={alt} />}
+      <div
+        className={`${avatarWidth[size]} rounded-full overflow-hidden  ${
+          loading ? "hidden" : ""
+        }`}
+      >
+        <img
+          src={src}
+          className={`h-full w-full object-cover`}
+          alt="avatar"
+          onLoad={() => {
+            setLoading(false);
+          }}
+        />
+      </div>
+    </>
   );
 };
 
