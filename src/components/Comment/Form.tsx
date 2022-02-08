9b3eb40e44347a34d93ee748f338f121b6ca005c
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-02-08 09:46:52 +0700

    chore: cancel text into discard in comment form

--- src/components/Comment/Form.tsx
+++ src/components/Comment/Form.tsx
@@ -417,10 +417,10 @@ const Form: React.FC<IProps> = ({
             {!isMobile && (
               <Button
                 type="submit"
-                className="mr-3 text-sm flex items-center justify-center bg-indigo-100 min-w-[5rem] text-black"
+                className="mr-3 text-sm flex items-center justify-center bg-neutral-100 min-w-[5rem] text-slate-700"
                 onClick={discardComment}
               >
-                Cancel
+                Discard
               </Button>
             )}
 
