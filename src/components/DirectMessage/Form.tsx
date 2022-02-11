4ab265fcdc431c1c4eb01947431b4a1a728d8cfc
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-02-11 11:56:07 +0700

    styling: message form UI

--- src/components/DirectMessage/Form.tsx
+++ src/components/DirectMessage/Form.tsx
@@ -1,8 +1,12 @@
 import { useRemirror } from "@remirror/react";
 import Button from "components/Button/Button";
+import IconButton from "components/Button/IconButton";
 import Remirror from "components/Remirror";
 import { extensions } from "components/Remirror/extensions";
 import React, { useRef } from "react";
+import { BiTrash } from "react-icons/bi";
+import { MdSend } from "react-icons/md";
+import { useMediaQuery } from "react-responsive";
 import { htmlToProsemirrorNode } from "remirror";
 
 type Props = {
@@ -15,6 +19,9 @@ interface EditorRef {
 }
 
 const MessageForm = ({ isShowEditor, setIsShowEditor }: Props) => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 600px)",
+  });
   const editorRef = useRef<EditorRef | null>(null);
 
   const {
@@ -68,21 +75,42 @@ const MessageForm = ({ isShowEditor, setIsShowEditor }: Props) => {
           <div className="flex justify-between -mt-9">
             <div />
             <div className="flex items-center py-2">
-              <Button
-                type="submit"
-                className="mr-3 text-sm flex items-center justify-center bg-indigo-100 min-w-[5rem] text-black"
-                onClick={discardComment}
-              >
-                Discard
-              </Button>
-
-              <Button
-                type="submit"
-                className="text-sm flex items-center justify-center bg-indigo-500 min-w-[5rem] text-white"
-                //   onClick={handleCreateComment}
-              >
-                Post
-              </Button>
+              {!isMobile && (
+                <Button
+                  type="submit"
+                  className="mr-3 text-sm flex items-center justify-center bg-indigo-100 min-w-[5rem] text-black"
+                  onClick={discardComment}
+                >
+                  Discard
+                </Button>
+              )}
+              {!isMobile && (
+                <Button
+                  type="submit"
+                  className="text-sm flex items-center justify-center bg-indigo-500 min-w-[5rem] text-white"
+                  //   onClick={handleCreateComment}
+                >
+                  Post
+                </Button>
+              )}
+              {isMobile && (
+                <IconButton
+                  size="medium"
+                  //   onClick={handleCreateComment}
+                  //   className="z-50"
+                >
+                  <BiTrash size={20} className="text-slate-500 " />
+                </IconButton>
+              )}
+              {isMobile && (
+                <IconButton
+                  size="medium"
+                  //   onClick={handleCreateComment}
+                  //   className="z-50"
+                >
+                  <MdSend size={20} className="text-indigo-500 " />
+                </IconButton>
+              )}
             </div>
           </div>
         </div>
--- src/components/DirectMessage/Header.tsx
+++ src/components/DirectMessage/Header.tsx
@@ -21,7 +21,7 @@ const MessageHeader = ({ data }: Props) => {
     <div className="w-full py-4 px-2 md:px-3 sticky top-0 grid grid-cols-2 border-b border-b-neutral-100 bg-white z-50">
       <div className="grid-cols-1 flex items-center gap-3">
         {isMobile && (
-          <IconButton size="small" onClick={() => setIsSidebarOpen(true)}>
+          <IconButton size="medium" onClick={() => setIsSidebarOpen(true)}>
             <BiArrowBack className="text-slate-800" size={20} />
           </IconButton>
         )}
