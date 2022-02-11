98e8979dfb482efef46e1618bd1bc4520e90c15c
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-02-11 21:35:10 +0700

    fix: parse special html element in ContentItem

--- src/components/ContentItem/ContentItem.tsx
+++ src/components/ContentItem/ContentItem.tsx
@@ -14,6 +14,7 @@ import { Menu } from "@headlessui/react";
 import { kontenbase } from "lib/client";
 import { useNavigate, useParams } from "react-router";
 import { useLongPress, LongPressDetectEvents } from "use-long-press";
+import { decode } from "html-entities";
 
 import IconButton from "components/Button/IconButton";
 import MenuItem from "components/Menu/MenuItem2";
@@ -207,8 +208,8 @@ function ContentItem({
         commentCreatedBy.firstName
       )} reopened this thread.`;
 
-    return `${getShortName(commentCreatedBy.firstName)} : ${editorToHTML(
-      commentState
+    return `${getShortName(commentCreatedBy.firstName)} : ${decode(
+      editorToHTML(commentState)
     )}`;
   };
 
